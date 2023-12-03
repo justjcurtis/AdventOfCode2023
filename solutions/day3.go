4f3b9b629ecf01eb65ee788f6c855afdae14a93e
justjcurtis <justjcurtis@gmail.com>
2023-12-03 19:50:08 +0000

    chore: clean up day3

--- solutions/day3.go
+++ solutions/day3.go
@@ -5,10 +5,8 @@ package solutions
 
 import (
 	"AdventOfCode2023/utils"
-	"fmt"
 	"strconv"
 	"strings"
-	"time"
 	"unicode"
 )
 
@@ -64,31 +62,25 @@ func GetNumbersFromSymbol(i int, j int, input []string) map[string][]int {
 	return nums
 }
 
-func GetRelevenNumbersFromLine(j int, input []string, isSymbol func(rune) bool) map[string][]int {
+func GetRelevenNumbersFromLine(j int, input []string) map[string][]int {
 	line := input[j]
 	nums := make(map[string][]int)
 	for i := 0; i < len(line); i++ {
 		char := rune(line[i])
-		if char == '.' {
+		if char == '.' || unicode.IsDigit(char) {
 			continue
 		}
-		if isSymbol(char) {
-			curr := GetNumbersFromSymbol(i, j, input)
-			nums = utils.MapAcc(nums, curr)
-		}
+		curr := GetNumbersFromSymbol(i, j, input)
+		nums = utils.MapAcc(nums, curr)
 	}
 	return nums
 }
 
 func GetReleventNumbers(input []string) map[string][]int {
 	fn := func(j int) map[string][]int {
-		return GetRelevenNumbersFromLine(j, input, isSymbol)
+		return GetRelevenNumbersFromLine(j, input)
 	}
 	return utils.Parallelise(utils.MapAcc[string, []int], fn, len(input))
-}
-
-func isSymbol(char rune) bool {
-	return !unicode.IsLetter(char) && !unicode.IsDigit(char) && char != '.'
 }
 
 func GetGears(input []string, nums map[string][]int) map[string][]int {
@@ -102,38 +94,24 @@ func GetGears(input []string, nums map[string][]int) map[string][]int {
 			gears[id] = append(gears[id], arr[0])
 		}
 	}
-	for key, arr := range gears {
-		if len(arr) != 2 {
-			delete(gears, key)
-		}
-	}
 	return gears
 }
 
 func Day3(input []string) []string {
-	start := time.Now()
 	nums := GetReleventNumbers(input)
-	elapsed := time.Since(start)
-	fmt.Printf("Time to get nums: %s\n", elapsed)
-	start = time.Now()
 	part1 := 0
 	for _, arr := range nums {
 		part1 += arr[0]
 	}
-	elapsed = time.Since(start)
-	fmt.Printf("Time to get part 1: %s\n", elapsed)
 
-	start = time.Now()
 	gears := GetGears(input, nums)
-	elapsed = time.Since(start)
-	fmt.Printf("Time to get gears: %s\n", elapsed)
-	start = time.Now()
 	part2 := 0
 	for _, arr := range gears {
+		if len(arr) != 2 {
+			continue
+		}
 		part2 += arr[0] * arr[1]
 	}
-	elapsed = time.Since(start)
-	fmt.Printf("Time to get part 2: %s\n\n", elapsed)
 
 	return []string{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
