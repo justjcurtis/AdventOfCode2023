77d0c9a0c951defef7b3692d517ba717cba3c945
justjcurtis <justjcurtis@gmail.com>
2023-12-04 02:20:18 +0000

    perf: improve parallelise speed

--- utils/parallelise.go
+++ utils/parallelise.go
@@ -3,19 +3,25 @@ Copyright © 2023 Jacson Curtis <justjcurtis@gmail.com>
 */
 package utils
 
-import "runtime"
+import (
+	"runtime"
+	"sync"
+)
 
 func Parallelise[T any](acc func(T, T) T, fn func(int) T, maxLength int) T {
 	var results T
 	workerCount := runtime.NumCPU() - 1
-	ch := make(chan T)
+	ch := make(chan T, workerCount)
+	wg := sync.WaitGroup{}
 	for i := 0; i < workerCount; i++ {
 		start := maxLength / workerCount * i
 		end := maxLength / workerCount * (i + 1)
 		if i == workerCount-1 {
 			end = maxLength
 		}
+		wg.Add(1)
 		go func(i int) {
+			defer wg.Done()
 			var result T
 			for j := start; j < end; j++ {
 				result = acc(result, fn(j))
@@ -23,9 +29,9 @@ func Parallelise[T any](acc func(T, T) T, fn func(int) T, maxLength int) T {
 			ch <- result
 		}(i)
 	}
+	wg.Wait()
 	for i := 0; i < workerCount; i++ {
-		current := <-ch
-		results = acc(results, current)
+		results = acc(results, <-ch)
 	}
 	return results
 }
