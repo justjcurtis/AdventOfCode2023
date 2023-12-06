e9ba5ac0135d35223dabaa789c3bb1bb4b8d6498
justjcurtis <justjcurtis@gmail.com>
2023-12-06 02:32:36 +0000

    test: basic day 5 solution tests

---
+++ solutions/day5_test.go
@@ -0,0 +1,60 @@
+/*
+Copyright © 2023 Jacson Curtis <justjcurtis@gmail.com>
+*/
+package solutions
+
+import "testing"
+
+var testDataDay5 = []string{
+	"seeds: 79 14 55 13",
+	"",
+	"seed-to-soil map:",
+	"50 98 2",
+	"52 50 48",
+	"",
+	"soil-to-fertilizer map:",
+	"0 15 37",
+	"37 52 2",
+	"39 0 15",
+	"",
+	"fertilizer-to-water map:",
+	"49 53 8",
+	"0 11 42",
+	"42 0 7",
+	"57 7 4",
+	"",
+	"water-to-light map:",
+	"88 18 7",
+	"18 25 70",
+	"",
+	"light-to-temperature map:",
+	"45 77 23",
+	"81 45 19",
+	"68 64 13",
+	"",
+	"temperature-to-humidity map:",
+	"0 69 1",
+	"1 0 69",
+	"",
+	"humidity-to-location map:",
+	"60 56 37",
+	"56 93 4",
+}
+
+func TestDay5Part1(t *testing.T) {
+	input := testDataDay5
+	result := Day5(input)[0]
+	expected := "35"
+	if result != expected {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
+
+func TestDay5Part2(t *testing.T) {
+	input := testDataDay5
+	result := Day5(input)[1]
+	expected := "46"
+	if result != expected {
+		t.Errorf("Expected %v, got %v", expected, result)
+	}
+}
