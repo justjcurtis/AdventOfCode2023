2886a1c9cd1b6e83e4490cc957b797299bd5db82
justjcurtis <justjcurtis@gmail.com>
2023-12-13 04:19:02 +0000

    wip: day 10 flood fill

--- solutions/day10.go
+++ solutions/day10.go
@@ -3,92 +3,95 @@ Copyright © 2023 Jacson Curtis <justjcurtis@gmail.com>
 */
 package solutions
 
-type day10Node struct {
-	x          int
-	y          int
-	isStart    bool
-	neighbours []*day10Node
-}
-
-type day10Parsed struct {
-	startX int
-	startY int
-	pipes  [][]day10Node
-}
+import (
+	"fmt"
+	"strconv"
+)
 
 var charMap = map[rune][][]int{
-	'|': {{0, 1}, {0, -1}},
-	'-': {{1, 0}, {-1, 0}},
-	'L': {{0, 1}, {1, 0}},
-	'J': {{0, 1}, {-1, 0}},
-	'7': {{0, -1}, {1, 0}},
-	'F': {{0, -1}, {-1, 0}},
-	'S': {{0, 1}, {0, -1}, {1, 0}, {-1, 0}},
+	'|': {{0, -1}, {0, 1}},                  // up down
+	'-': {{-1, 0}, {1, 0}},                  // left right
+	'L': {{0, -1}, {1, 0}},                  // up right
+	'J': {{0, -1}, {-1, 0}},                 // up left
+	'7': {{0, 1}, {-1, 0}},                  // down left
+	'F': {{0, 1}, {1, 0}},                   // down right
+	'S': {{0, -1}, {0, 1}, {-1, 0}, {1, 0}}, // all
 }
 
-func ParseDay10Input(input []string) day10Parsed {
-	startX := -1
-	startY := -1
-	neigbourMap := [][][][]int{}
-	pipes := [][]day10Node{}
+func FindStart(input []string) (int, int) {
 	for y, line := range input {
-		row := [][][]int{}
-		pipeRow := []day10Node{}
 		for x, char := range line {
-			if char == '.' {
-				row = append(row, [][]int{})
-			}
-			isStart := char == 'S'
-			if isStart {
-				startX = x
-				startY = y
-			}
-			row = append(row, charMap[char])
-			pipeRow = append(pipeRow, day10Node{x, y, isStart, []*day10Node{}})
-		}
-		neigbourMap = append(neigbourMap, row)
-	}
-	for y, row := range pipes {
-		for x, pipe := range row {
-			nearby := neigbourMap[y][x]
-			for _, dir := range nearby {
-				nx := x + dir[0]
-				ny := y + dir[1]
-				if nx < 0 || ny < 0 || nx >= len(pipes) || ny >= len(pipes[nx]) {
-					continue
-				}
-				pipe.neighbours = append(pipe.neighbours, &pipes[ny][nx])
+			if char == 'S' {
+				return x, y
 			}
 		}
 	}
-	return day10Parsed{startX, startY, pipes}
+	return -1, -1
 }
 
-func aStart(start *day10Node, end *day10Node) int {
-	visited := map[*day10Node]bool{}
-	gScore := map[*day10Node]int{}
-	fScore := map[*day10Node]int{}
-	queue := []*day10Node{start}
-	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
-		if visited[current] {
+func FloodRecursive(x int, y int, input []string, distance int, visited [][]int) (int, bool) {
+	neighbours := charMap[rune(input[y][x])]
+	visited[y][x] = distance
+	for _, neighbour := range neighbours {
+		nx, ny := x+neighbour[0], y+neighbour[1]
+		if nx < 0 || nx >= len(input[0]) || ny < 0 || ny >= len(input) {
 			continue
 		}
-		visited[current] = true
-		if current == end {
-			if len(visited) > 1 {
-				return 0
+		visitVal := visited[ny][nx]
+		if visitVal != 0 {
+			if visitVal == 1 && distance > 2 {
+				// for i := range visited {
+				// 	fmt.Println(visited[i])
+				// }
+				return distance, true
 			}
 		}
-		for _, neighbour := range current.neighbours {
-			queue = append(queue, neighbour)
+		char := input[ny][nx]
+		if char == '.' {
+			continue
+		}
+		maxDist, isLoop := FloodRecursive(nx, ny, input, distance+1, visited)
+		if isLoop {
+			return maxDist, true
 		}
 	}
+	return -1, false
+}
+
+func FloodFill(x int, y int, input []string) int {
+	results := []int{}
+	visited := make([][]int, len(input))
+	for i := range visited {
+		visited[i] = make([]int, len(input[0]))
+	}
+	neighbours := charMap['S']
+	visited[y][x] = 1
+	for _, neighbour := range neighbours {
+		nx, ny := x+neighbour[0], y+neighbour[1]
+		if nx < 0 || nx >= len(input[0]) || ny < 0 || ny >= len(input) {
+			continue
+		}
+		char := input[ny][nx]
+		if char == '.' {
+			continue
+		}
+		maxDist, isLoop := FloodRecursive(nx, ny, input, 2, visited)
+		if isLoop {
+			results = append(results, maxDist/2)
+		}
+	}
+	fmt.Println(results)
+	fmt.Println(visited)
 	return -1
 }
 
+func SolveDay10Part1(input []string) int {
+	x, y := FindStart(input)
+	result := FloodFill(x, y, input)
+	return result
+}
+
 func Day10(input []string) []string {
-	ParseDay10Input(input)
-	return []string{"", ""}
+	part1 := SolveDay10Part1(input)
+	return []string{strconv.Itoa(part1)}
 }
--- solutions/day10_test.go
+++ solutions/day10_test.go
@@ -5,5 +5,19 @@ package solutions
 
 import "testing"
 
+var testDataDay10 = []string{
+	"..F7.",
+	".FJ|.",
+	"SJ.L7",
+	"|F--J",
+	"LJ...",
+}
+
 func TestDay10Part1(t *testing.T) {
+	input := testDataDay10
+	result := Day10(input)[0]
+	expected := "8"
+	if result != expected {
+		t.Errorf("Expected %s, got %s", expected, result)
+	}
 }
