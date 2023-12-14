dad4a3b2507abe509a07583df6d76923b32d8308
justjcurtis <justjcurtis@gmail.com>
2023-12-14 15:44:27 +0000

    wip: day 10 part 2

--- solutions/day10.go
+++ solutions/day10.go
@@ -15,6 +15,8 @@ var charMap = map[rune][][]int{
 	'7': {{0, 1}, {-1, 0}},                  // down left
 	'F': {{0, 1}, {1, 0}},                   // down right
 	'S': {{0, -1}, {0, 1}, {-1, 0}, {1, 0}}, // all
+	'!': {{0, -1}, {0, 1}, {-1, 0}, {1, 0},
+		{-1, -1}, {1, 1}, {-1, 1}, {1, -1}}, // all + Diagonals
 }
 
 func FindStart(input []string) (int, int) {
@@ -55,7 +57,7 @@ func FloodRecursive(x int, y int, input []string, distance int, visited [][]int)
 	return -1, false
 }
 
-func FloodFill(x int, y int, input []string) int {
+func FloodFill(x int, y int, input []string) (int, [][]int) {
 	visited := make([][]int, len(input))
 	for i := range visited {
 		visited[i] = make([]int, len(input[0]))
@@ -77,20 +79,147 @@ func FloodFill(x int, y int, input []string) int {
 		if matchA || matchB {
 			maxDist, isLoop := FloodRecursive(nx, ny, input, 2, visited)
 			if isLoop {
-				return maxDist / 2
+				return maxDist / 2, visited
 			}
 		}
 	}
-	return -1
+	return -1, [][]int{}
 }
 
-func SolveDay10Part1(input []string) int {
+func SolveDay10Part1(input []string) (int, [][]int) {
 	x, y := FindStart(input)
-	result := FloodFill(x, y, input)
-	return result
+	return FloodFill(x, y, input)
+}
+
+var cornerPairs = map[rune]rune{
+	'L': '7',
+	'F': 'J',
+	'J': 'F',
+	'7': 'L',
+}
+
+func CanReachOutsideViaCrack(x int, y int, loopMap [][]int, input []string, outside [][]int) bool {
+	cardinal := charMap['S']
+	results := [4]int{}
+	lastVal := -1
+	waitFor := '.'
+	for i, dir := range cardinal {
+		nx, ny := x+dir[0], y+dir[1]
+		for nx >= 0 && nx < len(loopMap[0]) &&
+			ny >= 0 && ny < len(loopMap) {
+			val := loopMap[ny][nx]
+			if val == 0 {
+				if lastVal != -1 {
+					results[i]++
+					lastVal = -1
+				}
+				outVal := outside[ny][nx]
+				if outVal > 1 {
+					if results[i]%2 == 0 {
+						return outVal == 3
+					}
+					return outVal == 2
+				}
+			}
+			char := rune(input[ny][nx])
+			if val != lastVal-1 && val != lastVal+1 {
+				lastVal = val
+				if char != '|' && char != '-' {
+					waitFor = cornerPairs[char]
+				} else {
+					results[i]++
+				}
+				if nx == 0 || nx == len(loopMap[0])-1 ||
+					ny == 0 || ny == len(loopMap)-1 {
+					return results[i]%2 == 0
+				}
+			} else {
+				if waitFor != '.' {
+					if char != '|' && char != '-' {
+						waitFor = '.'
+						if char == waitFor {
+							results[i]++
+						}
+					}
+				}
+			}
+			nx += dir[0]
+			ny += dir[1]
+		}
+		results[i] /= 2
+	}
+	minimum := results[0]
+	for i := 1; i < 4; i++ {
+		if results[i] < minimum {
+			minimum = results[i]
+		}
+	}
+	return minimum%2 == 0
+}
+func CanReachEdge(x int, y int, loopMap [][]int, input []string, outside [][]int) bool {
+	if outside[y][x] == 1 {
+		return false
+	}
+	if outside[y][x] == 2 {
+		return false
+	}
+	if outside[y][x] == 3 ||
+		x == 0 || x == len(loopMap[0])-1 ||
+		y == 0 || y == len(loopMap)-1 {
+		outside[y][x] = 3
+		return true
+	}
+	outside[y][x] = 1
+	found := 2
+	neighbours := charMap['!']
+	for _, neighbour := range neighbours {
+		nx, ny := x+neighbour[0], y+neighbour[1]
+		if nx < 0 || nx >= len(loopMap[0]) ||
+			ny < 0 || ny >= len(loopMap) {
+			continue
+		}
+		if loopMap[ny][nx] == 0 {
+			if CanReachEdge(nx, ny, loopMap, input, outside) {
+				found = 3
+				break
+			}
+		}
+
+	}
+	if found == 99 {
+		if CanReachOutsideViaCrack(x, y, loopMap, input, outside) {
+			found = 3
+		}
+	}
+	outside[y][x] = found
+	for _, neighbour := range neighbours {
+		outside[y+neighbour[1]][x+neighbour[0]] = found
+	}
+	return found == 3
+}
+
+func SolveDay10Part2(loopMap [][]int, input []string) int {
+	w := len(loopMap[0])
+	outside := make([][]int, len(loopMap))
+	for i := range outside {
+		outside[i] = make([]int, w)
+	}
+	insideCount := 0
+	for y, line := range loopMap {
+		for x, val := range line {
+			if val != 0 {
+				continue
+			}
+			if !CanReachEdge(x, y, loopMap, input, outside) {
+				insideCount++
+			}
+		}
+	}
+	return insideCount
 }
 
 func Day10(input []string) []string {
-	part1 := SolveDay10Part1(input)
-	return []string{strconv.Itoa(part1)}
+	part1, visited := SolveDay10Part1(input)
+	part2 := SolveDay10Part2(visited, input)
+	return []string{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
--- solutions/day10_test.go
+++ solutions/day10_test.go
@@ -5,18 +5,59 @@ package solutions
 
 import "testing"
 
-var testDataDay10 = []string{
-	"..F7.",
-	".FJ|.",
-	"SJ.L7",
-	"|F--J",
-	"LJ...",
+var testDataDay10 = [][]string{
+	{
+		"..F7.",
+		".FJ|.",
+		"SJ.L7",
+		"|F--J",
+		"LJ...",
+	},
+	{
+		"...........",
+		".S-------7.",
+		".|F-----7|.",
+		".||.....||.",
+		".||.....||.",
+		".|L-7.F-J|.",
+		".|..|.|..|.",
+		".L--J.L--J.",
+		"...........",
+	},
+	{
+		"..........",
+		".S------7.",
+		".|F----7|.",
+		".||OOOO||.",
+		".||OOOO||.",
+		".|L-7F-J|.",
+		".|II||II|.",
+		".L--JL--J.",
+		"..........",
+	},
 }
 
 func TestDay10Part1(t *testing.T) {
-	input := testDataDay10
+	input := testDataDay10[0]
 	result := Day10(input)[0]
 	expected := "8"
+	if result != expected {
+		t.Errorf("Expected %s, got %s", expected, result)
+	}
+}
+
+func TestDay10Part2_1(t *testing.T) {
+	input := testDataDay10[1]
+	result := Day10(input)[1]
+	expected := "4"
+	if result != expected {
+		t.Errorf("Expected %s, got %s", expected, result)
+	}
+}
+func TestDay10Part2_2(t *testing.T) {
+	input := testDataDay10[2]
+	result := Day10(input)[1]
+	expected := "4"
 	if result != expected {
 		t.Errorf("Expected %s, got %s", expected, result)
 	}
