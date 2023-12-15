67b6add6557f2b2999f2ba16a42cf1fdcaa54cfc
justjcurtis <justjcurtis@gmail.com>
2023-12-15 05:18:09 +0000

    feat: day 11 boilerplate

---
+++ solutions/day11_test.go
@@ -0,0 +1,30 @@
+/*
+Copyright © 2023 Jacson Curtis <justjcurtis@gmail.com>
+*/
+package solutions
+
+import (
+	"testing"
+)
+
+var testDataDay11 = []string{
+	"...#......",
+	".......#..",
+	"#.........",
+	"..........",
+	"......#...",
+	".#........",
+	".........#",
+	"..........",
+	".......#..",
+	"#...#.....",
+}
+
+func TestDay11Part1(t *testing.T) {
+	input := testDataDay11
+	expected := "374"
+	actual := Day11(input)[0]
+	if actual != expected {
+		t.Errorf("Expected %s but was %s", expected, actual)
+	}
+}
