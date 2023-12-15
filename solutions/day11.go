a53f57a46636ba070b2a550515417d82202238b5
justjcurtis <justjcurtis@gmail.com>
2023-12-15 06:04:43 +0000

    wip: day 11

--- solutions/day11.go
+++ solutions/day11.go
@@ -3,6 +3,84 @@ Copyright © 2023 Jacson Curtis <justjcurtis@gmail.com>
 */
 package solutions
 
+import (
+	"AdventOfCode2023/utils"
+	"math"
+)
+
+func ExpandDay11(input []string) []string {
+	colsWithGalaxies := make([]bool, len(input[0]))
+	for i := len(input) - 1; i >= 0; i-- {
+		allDots := true
+		for j, char := range input[i] {
+			if char != '.' {
+				colsWithGalaxies[j] = true
+				allDots = false
+			}
+		}
+		if allDots {
+			input = append(input[:i+1], input[i:]...)
+		}
+	}
+	for j := len(input[0]) - 1; j >= 0; j-- {
+		isEmpty := !colsWithGalaxies[j]
+		if isEmpty {
+			for i := range input {
+				input[i] = input[i][:j+1] + input[i][j:]
+			}
+		}
+	}
+	return input
+}
+
+func HeuristicDay11(start []int, end []int) int {
+	return int(math.Abs(float64(start[0]-end[0])) + math.Abs(float64(start[1]-end[1])))
+}
+
+type QueueItem struct {
+	coords []int
+	score  int
+}
+
+func QueueItemComparator(a QueueItem, b QueueItem) bool {
+	return a.score < b.score
+}
+func QueueItemIsEqual(a QueueItem, b QueueItem) bool {
+	return a.coords[0] == b.coords[0] && a.coords[1] == b.coords[1]
+}
+
+func AstarDay11(start []int, end []int, input []string, h func([]int, []int) int) int {
+	openSet := utils.NewMinHeap[QueueItem](QueueItemComparator, QueueItemIsEqual)
+	openSet.Push(QueueItem{start, 0})
+	cameFrom := map[int][]int{}
+	gScore := map[int]int{}
+	fScore := map[int]int{}
+	gScore[utils.SzudzikPairing(start[0], start[1])] = 0
+	fScore[utils.SzudzikPairing(start[0], start[1])] = h(start, end)
+	for openSet.Len() > 0 {
+		current := openSet.Peek().coords
+		currentStr := utils.SzudzikPairing(current[0], current[1])
+		if current[0] == end[0] && current[1] == end[1] {
+			return gScore[currentStr]
+		}
+		openSet.Pop()
+		for _, neighbour := range GetNeighboursDay11(current, input) {
+			neighbourId := utils.SzudzikPairing(neighbour[0], neighbour[1])
+			tentativeGScore := gScore[currentStr] + 1
+			if tentativeGScore < gScore[neighbourId] {
+				cameFrom[neighbourId] = current
+				gScore[neighbourId] = tentativeGScore
+				fScore[neighbourId] = tentativeGScore + h(neighbour, end)
+				if !openSet.Contains(neighbour) {
+					openSet.Push(QueueItem{neighbour, fScore[neighbourId]})
+				}
+			}
+		}
+	}
+}
+
 func Day11(input []string) []string {
+	expanded := ExpandDay11(input)
+
 	return []string{}
 }
---
+++ utils/minHeap.go
@@ -0,0 +1,83 @@
+/*
+Copyright © 2023 Jacson Curtis <justjcurtis@gmail.com>
+*/
+package utils
+
+type MinHeap[T any] struct {
+	heap       []T
+	comparator func(T, T) bool
+	isEqual    func(T, T) bool
+}
+
+func NewMinHeap[T any](comparator func(T, T) bool, isEqual func(T, T) bool) *MinHeap[T] {
+	return &MinHeap[T]{comparator: comparator, isEqual: isEqual}
+}
+
+func (h *MinHeap[T]) Push(value T) {
+	h.heap = append(h.heap, value)
+	h.bubbleUp(len(h.heap) - 1)
+}
+
+func (h *MinHeap[T]) Pop() T {
+	last := len(h.heap) - 1
+	h.swap(0, last)
+	value := h.heap[last]
+	h.heap = h.heap[:last]
+	h.bubbleDown(0)
+	return value
+}
+
+func (h *MinHeap[T]) Peek() T {
+	return h.heap[0]
+}
+
+func (h *MinHeap[T]) bubbleUp(index int) {
+	parent := (index - 1) / 2
+	if parent < 0 || h.comparator(h.heap[parent], h.heap[index]) {
+		return
+	}
+	h.swap(index, parent)
+	h.bubbleUp(parent)
+}
+
+func (h *MinHeap[T]) bubbleDown(index int) {
+	left := index*2 + 1
+	right := index*2 + 2
+	if left >= len(h.heap) {
+		return
+	}
+	smallest := left
+	if right < len(h.heap) && h.comparator(h.heap[right], h.heap[left]) {
+		smallest = right
+	}
+	if h.comparator(h.heap[index], h.heap[smallest]) {
+		return
+	}
+	h.swap(index, smallest)
+	h.bubbleDown(smallest)
+}
+
+func (h *MinHeap[T]) swap(i int, j int) {
+	h.heap[i], h.heap[j] = h.heap[j], h.heap[i]
+}
+
+func (h *MinHeap[T]) Len() int {
+	return len(h.heap)
+}
+
+func (h *MinHeap[T]) IsEmpty() bool {
+	return h.Len() == 0
+}
+
+func (h *MinHeap[T]) Clear() {
+	h.heap = []T{}
+}
+
+func (h *MinHeap[T]) Contains(value T) bool {
+	for _, item := range h.heap {
+		if h.isEqual(item, value) {
+			return true
+		}
+	}
+	return false
+}
