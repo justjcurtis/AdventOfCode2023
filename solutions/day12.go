db81b340d90f45e4160a6c54145e403585b1a17c
justjcurtis <justjcurtis@gmail.com>
2023-12-19 20:14:43 +0000

    feat: day 12 part 2 complete

--- solutions/day12.go
+++ solutions/day12.go
@@ -9,113 +9,128 @@ import (
 	"strings"
 )
 
-func ParseLineDay12(line string) (string, []int, int) {
+func ParseLineDay12(line string) (string, []int) {
 	arr := strings.Split(line, " ")
-	wilds := 0
-	for _, char := range arr[0] {
-		if char == '?' {
-			wilds++
-		}
-	}
 	numStrs := strings.Split(arr[1], ",")
 	nums := make([]int, len(numStrs))
 	for i, numStr := range numStrs {
 		nums[i], _ = strconv.Atoi(numStr)
 	}
-	return arr[0], nums, wilds
+	return arr[0], nums
 }
 
-func ParseDay12(input []string) ([]string, [][]int, []int) {
+func ParseDay12(input []string) ([]string, [][]int) {
 	chars := make([]string, len(input))
 	nums := make([][]int, len(input))
-	wilds := make([]int, len(input))
 	fn := func(j int) {
-		char, num, wild := ParseLineDay12(input[j])
+		char, num := ParseLineDay12(input[j])
 		chars[j] = char
 		nums[j] = num
-		wilds[j] = wild
 	}
 	utils.ParalleliseVoid(fn, len(input))
-	return chars, nums, wilds
+	return chars, nums
 }
 
-func SolveDay12Line(
-	wilds int, chars string, nums []int,
-	lastChar rune, index int, groupIndex int, groups []int) int {
-	if index == len(chars) {
-		if len(groups) < len(nums) {
+func GetIdDay12(chars string, nums []int) string {
+	id := chars
+	for _, num := range nums {
+		id += "," + strconv.Itoa(num)
+	}
+	return id
+}
+
+var day12Cache = make(map[string]int)
+
+func SolveDay12Line(chars string, nums []int) int {
+	id := GetIdDay12(chars, nums)
+	if val, ok := day12Cache[id]; ok {
+		return val
+	}
+	if len(chars) == 0 {
+		if len(nums) == 0 {
+			return 1
+		}
+		return 0
+	}
+	if len(nums) == 0 {
+		if strings.Contains(chars, "#") {
 			return 0
 		}
-		for i := groupIndex; i < len(nums); i++ {
-			if groups[i] != nums[i] {
-				return 0
-			}
-		}
 		return 1
 	}
-	if len(groups) < groupIndex+1 {
-		groups = append(groups, 0)
-	}
-	if groupIndex >= len(nums) {
-		for i := index; i < len(chars); i++ {
-			if rune(chars[i]) == '#' {
-				return 0
-			}
-		}
-		return 1
-	}
+	char := rune(chars[0])
 	result := 0
-	switch rune(chars[index]) {
+	switch char {
 	case '#':
-		if groups[groupIndex] == nums[groupIndex] {
-			return 0
+		if nums[0] <= len(chars) &&
+			!strings.Contains(chars[:nums[0]], ".") &&
+			(nums[0] == len(chars) || chars[nums[0]] != '#') {
+			nextChars := ""
+			if nums[0]+1 < len(chars) {
+				nextChars = chars[nums[0]+1:]
+			}
+			result = SolveDay12Line(nextChars, nums[1:])
+			break
 		}
-		nextGroups := make([]int, len(groups))
-		copy(nextGroups, groups)
-		nextGroups[groupIndex]++
-		return SolveDay12Line(wilds, chars, nums, '#', index+1, groupIndex, nextGroups)
+		result = 0
+		break
 	case '.':
-		nextGroupIndex := groupIndex
-		if index > 0 && lastChar == '#' {
-			if groups[groupIndex] < nums[groupIndex] {
-				return 0
-			}
-			nextGroupIndex++
-		}
-		nextGroups := make([]int, len(groups))
-		copy(nextGroups, groups)
-		return SolveDay12Line(wilds, chars, nums, '.', index+1, nextGroupIndex, nextGroups)
+		result = SolveDay12Line(chars[1:], nums)
+		break
 	case '?':
-		if groups[groupIndex] < nums[groupIndex] {
-			nextGroups := make([]int, len(groups))
-			copy(nextGroups, groups)
-			nextGroups[groupIndex]++
-			result = SolveDay12Line(wilds-1, chars, nums, '#', index+1, groupIndex, nextGroups)
-		}
-		nextGroupIndex := groupIndex
-		if index > 0 && lastChar == '#' {
-			if groups[groupIndex] < nums[groupIndex] {
-				return result
+		if nums[0] <= len(chars) &&
+			!strings.Contains(chars[:nums[0]], ".") &&
+			(nums[0] == len(chars) || chars[nums[0]] != '#') {
+			nextChars := ""
+			if nums[0]+1 < len(chars) {
+				nextChars = chars[nums[0]+1:]
 			}
-			nextGroupIndex++
+			result += SolveDay12Line(nextChars, nums[1:])
 		}
-		otherNextGroups := make([]int, len(groups))
-		copy(otherNextGroups, groups)
-		result += SolveDay12Line(wilds-1, chars, nums, '.', index+1, nextGroupIndex, otherNextGroups)
-		return result
+		result += SolveDay12Line(chars[1:], nums)
+		break
 	}
-	return -1
+	day12Cache[id] = result
+	return result
 }
 
-func SolveDay12Part1(chars []string, nums [][]int, wilds []int) int {
-	fn := func(j int) int {
-		return SolveDay12Line(wilds[j], chars[j], nums[j], '.', 0, 0, []int{})
+func SolveDay12Part1(chars []string, nums [][]int) int {
+	total := 0
+	for i := 0; i < len(chars); i++ {
+		total += SolveDay12Line(chars[i], nums[i])
 	}
-	return utils.Parallelise(utils.IntAcc, fn, len(chars))
+	return total
+}
+
+func UnfoldDay12Line(chars string, nums []int) (string, []int) {
+	resultChars := strings.Join([]string{
+		chars, chars, chars, chars, chars,
+	}, "?")
+	resultNums := make([]int, len(nums)*5)
+	for i := 0; i < 5; i++ {
+		for j, num := range nums {
+			resultNums[i*len(nums)+j] = num
+		}
+	}
+	return resultChars, resultNums
+}
+
+func SolveDay12Part2Line(chars string, nums []int) int {
+	unfoldedChars, unfoldedNums := UnfoldDay12Line(chars, nums)
+	return SolveDay12Line(unfoldedChars, unfoldedNums)
+}
+
+func SolveDay12Part2(chars []string, nums [][]int) int {
+	total := 0
+	for i := 0; i < len(chars); i++ {
+		total += SolveDay12Part2Line(chars[i], nums[i])
+	}
+	return total
 }
 
 func Day12(input []string) []string {
-	chars, nums, wilds := ParseDay12(input)
-	part1 := SolveDay12Part1(chars, nums, wilds)
-	return []string{strconv.Itoa(part1)}
+	chars, nums := ParseDay12(input)
+	part1 := SolveDay12Part1(chars, nums)
+	part2 := SolveDay12Part2(chars, nums)
+	return []string{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
